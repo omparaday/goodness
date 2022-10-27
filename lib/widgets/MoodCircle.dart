3030a048ec87d2b4c68ebe1b7bb744c71943284d
Omprakash <omparaday@gmail.com>
2022-10-27 12:38:55 +0530

    Removed words and dependent code

--- lib/widgets/MoodCircle.dart
+++ lib/widgets/MoodCircle.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:goodness/widgets/DecoratedText.dart';
 import 'package:sprintf/sprintf.dart';
 
 import '../home.dart';
